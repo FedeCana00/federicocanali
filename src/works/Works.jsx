6423eb3f2ccfacfff3661fd848d163c10e99bc41
FedeCana00 <federico.canali00@gmail.com>
2022-04-17 19:07:19 +0200

    Add buttons to each project.

--- src/works/Works.jsx
+++ src/works/Works.jsx
@@ -7,13 +7,17 @@ export default function Works() {
             type: "Android application",
             name: "Easy bill at the table",
             description: "You are at a restaurant with friends and it is time for the bill. You only have cash available. Don't you know how to divide the money between you so that everyone has the change? This app will solve your problems!",
-            img: "/assets/projects/easy-bill-at-table.png"
+            img: "/assets/projects/easy-bill-at-table.png",
+            source_code: "",
+            demo: "https://play.google.com/store/apps/details?id=com.MobDevFC.friendsdinnerbill"
         },
         {
             type: "Desktop application",
             name: "Electronic e-commerce",
             description: "Electronic e-commerce developed in java. There are three types of users who can use the desktop app: administrator, employee, and customer. Warehouse products can be bought, shipped, sold and managed.",
-            img: "/assets/projects/electronic-ecommerce.png"
+            img: "/assets/projects/electronic-ecommerce.png",
+            source_code: "https://github.com/FedeCana00/Electronic-online-store-javaFX/tree/main",
+            demo: ""
         },
     ];
 
@@ -35,6 +39,10 @@ export default function Works() {
                             <p>
                                 {value.description}
                             </p>
+                            <div className="buttons-section">
+                                <a href=""><button className="outline">View source code</button></a>
+                                <a href=""><button>Demo</button></a>                 
+                            </div>
                         </div>
                         <div className="right">
                             <div className="image-container">
