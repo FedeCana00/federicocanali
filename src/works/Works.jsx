ebd71988a285728d70a15e34be2d0f68603cb380
FedeCana00 <federico.canali00@gmail.com>
2022-04-17 23:41:05 +0200

    Complete projects component.

--- src/works/Works.jsx
+++ src/works/Works.jsx
@@ -1,25 +1,7 @@
 import "./works.scss";
+import { projects } from "./projects";
 
 export default function Works() {
-
-    const projects = [
-        {
-            type: "Android application",
-            name: "Easy bill at the table",
-            description: "You are at a restaurant with friends and it is time for the bill. You only have cash available. Don't you know how to divide the money between you so that everyone has the change? This app will solve your problems!",
-            img: "/assets/projects/easy-bill-at-table.png",
-            source_code: "",
-            demo: "https://play.google.com/store/apps/details?id=com.MobDevFC.friendsdinnerbill"
-        },
-        {
-            type: "Desktop application",
-            name: "Electronic e-commerce",
-            description: "Electronic e-commerce developed in java. There are three types of users who can use the desktop app: administrator, employee, and customer. Warehouse products can be bought, shipped, sold and managed.",
-            img: "/assets/projects/electronic-ecommerce.png",
-            source_code: "https://github.com/FedeCana00/Electronic-online-store-javaFX/tree/main",
-            demo: ""
-        },
-    ];
 
     // show or hide demo project button
     function showDemoButton(demoUrl){;
---
+++ src/works/projects.js
@@ -0,0 +1,75 @@
+
+    export const projects = [
+        {
+            type: "Android application",
+            name: "Easy bill at the table",
+            description: "You are at a restaurant with friends and it is time for the bill. You only have cash available. Don't you know how to divide the money between you so that everyone has the change? This app will solve your problems!",
+            img: "/assets/projects/easy-bill-at-table.png",
+            source_code: "",
+            demo: "https://play.google.com/store/apps/details?id=com.MobDevFC.friendsdinnerbill"
+        },
+        {
+            type: "Desktop application",
+            name: "Electronic e-commerce",
+            description: "Electronic e-commerce developed in java. There are three types of users who can use the desktop app: administrator, employee, and customer. Warehouse products can be bought, shipped, sold and managed.",
+            img: "/assets/projects/electronic-ecommerce.png",
+            source_code: "https://github.com/FedeCana00/Electronic-online-store-javaFX/tree/main",
+            demo: ""
+        },
+        {
+            type: "Android application",
+            name: "Find my parked car",
+            description: "Remembering where you parked and finding your car is simple, fast and effective! With this android app you can see your position and that of the car on the maps. Let yourself be guided by maps and GPS or rely on the navigation of Google Maps, Waze etc.",
+            img: "/assets/projects/find-my-car.png",
+            source_code: "",
+            demo: "https://play.google.com/store/apps/details?id=com.MobDevFC.findmyparkedcar"
+        },
+        {
+            type: "Website",
+            name: "Pay per view",
+            description: "E-commerce selling films similar to Google Play films.",
+            img: "/assets/projects/google-play-film.png",
+            source_code: "https://github.com/FedeCana00/Pay-per-view",
+            demo: "https://pay-per-view-website.netlify.app/"
+        },
+        {
+            type: "Android application",
+            name: "Green Pass Wallet",
+            description: "In this application you will always have your Green Pass with you. With one click you will be immediately ready to show your Green Pass with the brightness of the mobile phone screen at MAXIMUM. Upload your QR code to the app from gallery, pdf or camera. Cut out your Green Pass to make it best suit your preferences.",
+            img: "/assets/projects/green-pass.png",
+            source_code: "",
+            demo: "https://play.google.com/store/apps/details?id=com.MobDevFC.greenpasswallet"
+        },
+        {
+            type: "Android application",
+            name: "Loyalty cards",
+            description: "Why have a lot of cards in your wallet when you can always have them with you on your phone? Why waste time looking for your card among a thousand cards when you could find it with a click?",
+            img: "/assets/projects/loyalty-cards.png",
+            source_code: "",
+            demo: "https://play.google.com/store/apps/details?id=com.MobDevFC.LoyaltyCards"
+        },
+        {
+            type: "Android application",
+            name: "Extraction of numbers and people",
+            description: "Extraction of numbers and people is an easy and intuitive App to extract a random number (or random numbers) and people from a list. The App can be used to extract the students questioned during the lesson of mathematics, physics, geometry, Italian, English, ... Do you want to draw who will offer dinner at the restaurant? Or would you like to create a soccer team? Then use Extraction of numbers and people!",
+            img: "/assets/projects/numbers-people-extraction.png",
+            source_code: "",
+            demo: "https://play.google.com/store/apps/details?id=com.mobdevfc.extractionsofnumbers"
+        },
+        {
+            type: "Android application",
+            name: "Smart Working",
+            description: "Easily check the percentages assigned to you with both hourly and day management. The percentages will be calculated in real time at each insertion of a working day. A pie chart will show the percentages of hours spent in smart working and in the office. Enter the percentages of work from home and in the office based on the needs of your employer and choose whether to calculate them for a month or for a quarter.",
+            img: "/assets/projects/smart-working.png",
+            source_code: "",
+            demo: "https://play.google.com/store/apps/details?id=com.mobdevfc.smartworkinghour"
+        },
+        {
+            type: "Desktop application",
+            name: "Space Invaders",
+            description: "Arcade video game developed in python with Pygame.",
+            img: "/assets/projects/space-invaders.png",
+            source_code: "https://github.com/FedeCana00/Space-Invaders",
+            demo: ""
+        },
+    ];
