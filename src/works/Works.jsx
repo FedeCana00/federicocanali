fe66c656acb7adfc885a37eecb0e2129be38bfe6
FedeCana00 <federico.canali00@gmail.com>
2022-04-17 23:14:17 +0200

    Fixed bug of project image and buttons section.

--- src/works/Works.jsx
+++ src/works/Works.jsx
@@ -21,6 +21,22 @@ export default function Works() {
         },
     ];
 
+    // show or hide demo project button
+    function showDemoButton(demoUrl){;
+        if(demoUrl != "")
+            return (<a href={demoUrl}><button>Demo</button></a> );
+        
+        return;
+    }
+
+    // show or hide source code project button
+    function showSourceCodeButton(sourceCodeUrl){
+        if(sourceCodeUrl != "")
+            return (<a href={sourceCodeUrl}><button className="outline">View source code</button></a>);
+        
+        return;
+    }
+
     return (
         <div className="works">
             <h1>
@@ -40,8 +56,8 @@ export default function Works() {
                                 {value.description}
                             </p>
                             <div className="buttons-section">
-                                <a href=""><button className="outline">View source code</button></a>
-                                <a href=""><button>Demo</button></a>                 
+                                {showSourceCodeButton(value.source_code)}
+                                {showDemoButton(value.demo)}   
                             </div>
                         </div>
                         <div className="right">
