37cd4df442670507e7b326ed021222a37f70cf0a
federicocanali <federico.canali00@gmail.com>
2022-08-21 18:00:17 +0200

    Fixed bug in footer Material UI icons.

--- src/social-media/SocialMedia.jsx
+++ src/social-media/SocialMedia.jsx
@@ -1,11 +1,13 @@
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
+import "./socialMedia.scss";
+
 export default function SocialMedia({isFooter}) {
 
     let GITHUB_URL = "https://github.com/FedeCana00";
     let LINKEDIN_UL = "https://www.linkedin.com/in/federico-canali-26b004211";
-    let iconColor = isFooter ? "#fff" : "c1332a";
+    let iconColor = isFooter ? "#fff" : "#c1332a";
 
     return(
         <div className="social-container">
